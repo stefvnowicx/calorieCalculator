04c78eef4db1843abb5ab5a122ecf20d588727ab
Mateusz <stefvnowicx@gmail.com>
2025-02-26 19:00:03 +0100

    dodac data-value oraz paragrafy error

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const allPages = document.querySelectorAll(".page");
 const allBtns = document.querySelectorAll(".page__btn");
+let translateY = 100;
 
+const nextPage = (params) => {
+   allPages.forEach((page) => {
+      page.style.transform = `translateY(-${translateY}%)`;
+   });
+   translateY += 100;
+};
+
+allBtns.forEach((btn) => {
+   btn.addEventListener("click", () => {
+       if (btn.classList.contains("restart-btn")) {
+          window.location.reload();
+       }
+      nextPage();
+      console.log(btn.dataset.value);
+   });
+});
